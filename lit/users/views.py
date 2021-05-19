b49c8bef14e2911adbcfbda7967206b7e559c697
Madhumita V <madhumitav24@gmail.com>
2021-05-19 19:52:14 +0530

    Added files

---
+++ lit/users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm, UserUpdateForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f'Account successfully created!')
+            return redirect('login')
+    else:
+        form=UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        update_form = UserUpdateForm(request.POST, instance=request.user)
+        if update_form.is_valid():
+            update_form.save()
+            messages.success(request,f'Account successfully updated!')
+            return redirect('profile')
+    else:
+        update_form = UserUpdateForm(instance=request.user)
+    context = {
+        'update_form': update_form
+    }
+
+    return render(request, 'users/profile.html', context)
