2d67017efff2eea3daf9fcfa136d084beb91ec27
Madhumita V <71038264+vmm24@users.noreply.github.com>
2021-07-14 20:18:38 +0530

    Update settings.py

--- lit/lit/settings.py
+++ lit/lit/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
